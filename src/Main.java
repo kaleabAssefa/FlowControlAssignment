5f36e0138682455c9967b6c5df618fba957761b0
kaleabAssefa <62577860+kaleabAssefa@users.noreply.github.com>
2026-07-31 14:47:11 -0400

    Implement digit counting in Main.java
    Added a for loop to count the digits of a given number.

--- src/Main.java
+++ src/Main.java
@@ -48,8 +48,13 @@ public class Main {
             System.out.println(number+" x "+ i + " = "+(number*i));
         }
 
-
-
+        //7. Java program to count the digits of a given number using for loop.
+        int count=0;
+        int num = 25;
+        for (; num != 0; num = num / 10) {
+            count++;
+        }
+        System.out.println("Number of digits: " + count);
 
     }
 }
